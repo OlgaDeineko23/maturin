0bcf6ba32a13914067208e766cdd3c868f4c6c2f
OlgaDeineko <olga_deineko@ukr.net>
2018-05-05 17:20:10 +0300

    category page(scroll), position of modal

--- js/scroll-down.js
+++ js/scroll-down.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
 
     $('.masonry-layout__panel').each( function(i){
 
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight()-300;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
-
       if( bottom_of_window > bottom_of_object ){
 
         $(this).animate({'opacity':'1'},500);
